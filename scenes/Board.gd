89d2c0592ae134169bc99b1e82613eb901475724
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-11-12 10:30:16 +0100

    board scene

---
+++ scenes/Board.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export (int) var rowsNum = 14
+export (int) var colsNum = 24
+var rows
+
+export (float) var cellSize = 20.0
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
