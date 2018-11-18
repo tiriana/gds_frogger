c995db928b8da791396de141feeeb56790fcc624
Radomir Wojtera <radomir.wojtera@gmail.com>
2018-11-18 23:51:58 +0100

    Frog movement DONE

--- scenes/Frog.gd
+++ scenes/Frog.gd
@@ -1,67 +1,73 @@
 extends Node2D
 
 export (int) var speed  # How fast the player will move (pixels/sec).
-var screensize  # Size of the game window.
 var velocity = Vector2()
 
-var input_to_v_and_r = {
-	"ui_right": {
-		velocity: Vector2(1, 0),
-		rotation: PI / 2
+var directions = {
+	"up": {
+		"velocity": Vector2(0, -1),
+		"rotation": 0,
+		"ui": "ui_up",
+		"priority": 0
 	},
-	"ui_left": {
-		velocity: Vector2(-1, 0),
-		rotation: -PI / 2
+	"down": {
+		"velocity": Vector2(0, 1),
+		"rotation": PI,
+		"ui": "ui_down",
+		"priority": 0
 	},
-	"ui_down": {
-		velocity: Vector2(0, 1),
-		rotation: PI
+	"left": {
+		"velocity": Vector2(-1, 0),
+		"rotation": -PI / 2,
+		"ui": "ui_left",
+		"priority": 0
 	},
-	"ui_up": {
-		velocity: Vector2(0, -1),
-		rotation: 0
+	"right": {
+		"velocity": Vector2(1, 0),
+		"rotation": PI / 2,
+		"ui": "ui_right",
+		"priority": 0
 	}
 }
 
+var highest_priority = 0
+var current_direction = {
+		"velocity": Vector2(0, 0),
+		"rotation": 0
+	}
+
 func _ready():
 	pass
+	
+func _find_highest_direction():
+	var highest = {
+		"velocity": Vector2(0, 0),
+		"priority": 0,
+		"rotation": rotation
+	}
+	for direction in directions.values():
+		if direction["priority"] > highest["priority"]:
+			highest = direction
+	return highest
 
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-		rotation = PI / 2
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-		rotation = -PI / 2
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
-		rotation = PI
-	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-		rotation = 0
-		
-	if Input.is_action_just_released("ui_right"):
-		velocity.x = 0
-	if Input.is_action_just_released("ui_left"):
-		velocity.x = 0
-	if Input.is_action_just_released("ui_down"):
-		velocity.y = 0
-	if Input.is_action_just_released("ui_up"):
-		velocity.y = 0
-		
-	if Input.is_action_just_pressed("ui_right"):
-		velocity.y = 0
-	if Input.is_action_just_pressed("ui_left"):
-		velocity.y = 0
-	if Input.is_action_just_pressed("ui_down"):
-		velocity.x = 0
-	if Input.is_action_just_pressed("ui_up"):
-		velocity.x = 0
-		
+	for direction in directions.values():
+		if Input.is_action_just_released(direction["ui"]):
+			direction["priority"] = 0;
+			current_direction = _find_highest_direction()
+			highest_priority = current_direction["priority"]
+		if Input.is_action_just_pressed(direction["ui"]):
+			highest_priority += 1
+			direction["priority"] = highest_priority
+			current_direction = direction
+			
+	velocity = current_direction["velocity"]
+	rotation = current_direction["rotation"]
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
         $AnimatedSprite.stop()
-		
+
 	position += velocity * delta
